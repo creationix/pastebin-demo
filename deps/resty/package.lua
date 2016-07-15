ddbd05b57020286a5d3ba9efe513b656e00588d5
Tim Caswell <tim@creationix.com>
2016-07-15 12:37:33 -0500

    Include all of resty-template files

---
+++ deps/resty/package.lua
@@ -0,0 +1,11 @@
+return {
+  name = "creationix/lua-resty-template",
+  version = "dev-1",
+  homepage = "https://github.com/bungle/lua-resty-template",
+  author = "Aapo Talvensaari",
+  license = "MIT",
+  files = {
+    "template.lua",
+    "template/html.lua",
+  }
+}
--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local bundle = require('luvi').bundle
 loadstring(bundle.readfile("luvit-loader.lua"), "bundle:luvit-loader.lua")()
-local template = require('resty-template')
+local template = require('resty/template')
 
 template.render("www/layout.html", {
   title = "Testing lua-resty-template",
